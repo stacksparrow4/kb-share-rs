812a9cbf7fcf5eba1c395d60b3351a25c07caae2
stacksparrow4 <stacksparrow4@gmail.com>
2022-10-30 18:08:46 +1100

    made stable for test build

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,9 +1,9 @@
-use enigo::*;
+// use enigo::*;
 
 pub fn presskeydown() {
-    let mut enigo = Enigo::new();
+    // let mut enigo = Enigo::new();
 
     // enigo.key_down("asd");
 
-    enigo.mouse_move_to(500, 200);
+    // enigo.mouse_move_to(500, 200);
 }
